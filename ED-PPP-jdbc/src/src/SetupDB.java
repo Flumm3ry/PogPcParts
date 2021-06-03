ccfedbc68d580c5d0e5b52852145ad7071e569cb
Jeremy Maxwell <jeremy.v.maxwell@bigpond.com>
2021-06-03 14:08:15 +1000

    Add the login stuff

--- ED-PPP-jdbc/src/src/SetupDB.java
+++ ED-PPP-jdbc/src/src/SetupDB.java
@@ -72,13 +72,13 @@ public class SetupDB {
 
         // prepare data
         User user001 = new User("Adam", "1234567890", "1 John Street, Hawthorn",
-                "adam@secure.com.au", "ee79976c9380d5e337fc1c095ece8c8f22f91f306ceeb161fa51fecede2c4ba1", "ED-APP-ADMIN", true);
+                "adam@secure.com.au", "ee79976c9380d5e337fc1c095ece8c8f22f91f306ceeb161fa51fecede2c4ba1", "ADMIN", true);
         User user002 = new User("Bill", "2345678901", "2 Paul Street, Hawthorn",
-                "bill@secure.com.au", "33A7D3DA476A32AC237B3F603A1BE62FAD00299E0D4B5A8DB8D913104EDEC629", "ED-APP-ADMIN", true);
+                "bill@secure.com.au", "33A7D3DA476A32AC237B3F603A1BE62FAD00299E0D4B5A8DB8D913104EDEC629", "ADMIN", true);
         User user003 = new User("Ceci", "3456789012", "3 Mary Street, Hawthorn",
-                "ceci@secure.com.au", "AFB47E00531153E93808589E43D02C11F6398C5BC877F7924CEBCA8211C8DD18", "ED-APP-USERS", true);
+                "ceci@secure.com.au", "AFB47E00531153E93808589E43D02C11F6398C5BC877F7924CEBCA8211C8DD18", "USER", true);
         User user004 = new User("Dave", "4567890123", "4 Pete Street, Hawthorn",
-                "dave@secure.com.au", "B3C4B40750A97212E8981E4AC494D1EC77053F1EAF4E0934C276B74FC4F87C48", "ED-APP-USERS", true);
+                "dave@secure.com.au", "B3C4B40750A97212E8981E4AC494D1EC77053F1EAF4E0934C276B74FC4F87C48", "USER", true);
 
         // prepare list
         ArrayList<User> userList = new ArrayList<>();
