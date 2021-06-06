3e08b516d9f7e0b0241380a84cf6336a2ad69176
Jeremy Maxwell <jeremy.v.maxwell@bigpond.com>
2021-06-07 01:22:09 +1000

    Fix some more bugs

--- ED-PPP/ED-PPP-ejb/src/java/session/UserFacade.java
+++ ED-PPP/ED-PPP-ejb/src/java/session/UserFacade.java
@@ -25,7 +25,7 @@ import javax.xml.bind.DatatypeConverter;
  * @author jerem
  */
 @DeclareRoles({"ADMIN", "USER"})
-@Stateless
+@Stateless(mappedName="ejb/UserFacadeRemote")
 public class UserFacade implements UserFacadeRemote {
     
     @PersistenceContext
